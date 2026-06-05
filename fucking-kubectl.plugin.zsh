1132930b9229b1e9fef678e7c912e0b4f07b1081
Paul Harkink <paul@paulharkink.nl>
2026-06-05 14:12:24 +0200

    Fix oh-my-zsh plugin completions

--- fucking-kubectl.plugin.zsh
+++ fucking-kubectl.plugin.zsh
@@ -2,3 +2,9 @@ plugin_dir="${0:A:h}"
 
 export PATH="$plugin_dir/bin:$PATH"
 fpath=("$plugin_dir/completions" $fpath)
+
+if (( $+functions[compdef] )); then
+  autoload -Uz _please _fucking
+  compdef _please please
+  compdef _fucking fucking
+fi
